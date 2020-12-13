21a1978a95bcb6597210c015dcc8fcd00cccfd1e
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-12-13 05:51:12 -0800

    Update cekserver.php

--- cekserver.php
+++ cekserver.php
@@ -15,9 +15,9 @@ echo PHP_OS;
 Cek User linux di server / hosting<br>
 <?php
 for ($uid = 0; $uid < 5000; $uid++) { 
-  $nothing = posix_getpwuid($uid); 
-  if (!empty($nothing)) { 
-    while (list($kunci, $cek) = each($nothing)) { print "{$cek}:"; } print "<br />"; 
+  $x = posix_getpwuid($uid); 
+  if (!empty($x)) { 
+    while (list($kunci, $cek) = each($x)) { print "{$cek}:"; } print "<br />"; 
   }
 }
 ?>
